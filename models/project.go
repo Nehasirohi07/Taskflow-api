525dde766fca7ef87f756b5380617b7c84d45da6
nehasirohi07 <nehasirohi665@gmail.com>
2026-07-20 18:27:01 +0530

    feat: add project model

--- models/project.go
+++ models/project.go
@@ -1,1 +1,15 @@
 package models
+
+import (
+	"time"
+)
+
+type Project struct {
+	ID          int       `json:"id"`
+	UserID      int       `json:"user_id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
