106b066c6435887cedb1353cf8bbe7c4c74c6bd1
nehasirohi07 <nehasirohi665@gmail.com>
2026-07-20 18:33:37 +0530

    feat: add task model

--- models/task.go
+++ models/task.go
@@ -1,1 +1,17 @@
 package models
+
+import (
+	"time"
+)
+
+type Task struct {
+	ID          int       `json:"id"`
+	ProjectID   int       `json:"project_id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Priority    string    `json:"priority"`
+	Status      string    `json:"status"`
+	DueDate     time.Time `json:"due_date"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
